17ebc2ea6fac6e5846e6d9f83065f91003ca324f
serkantoraman <serkan@github.com>
2023-05-16 21:16:24 +0300

    Done Date format and bg color

--- src/Task.js
+++ src/Task.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { differenceInDays,formatDistanceToNow } from 'date-fns';
+import { tr } from 'date-fns/locale';
 
 const Task = ({ taskObj, onComplete }) => {
+
+    const deadlineDate = new Date(taskObj.deadline);
+    const formatDistanceResult = formatDistanceToNow(deadlineDate,
+      {addSuffix: true,
+      locale: tr},
+    );
+    
+   const bgClass = differenceInDays(deadlineDate ,new Date())<=3 ? "bg-[#ffd9d4]" : "bg-[#d2d5fd]";
+
+    
+    
 
   return (
     <div className="task">
       <h3>{taskObj.title}</h3>
-      <div className="deadline">son teslim: <span>{taskObj.deadline}</span></div>
+      <div className="deadline">son teslim: <span className={bgClass}>{formatDistanceResult}</span></div>
       <p>{taskObj.description}</p>
       <div>
         {taskObj.people.map((p) => (
